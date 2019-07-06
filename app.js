84380277b57d80cf5fbd7682fae06853247295f3
Brian <baccarellab@gmail.com>
2019-07-06 16:09:27 -0400

    fix bug
    Bug causing sleep and rest to not show
    
    Add to log was not working

--- app.js
+++ app.js
@@ -11,9 +11,11 @@ function Workout(date, bodyWeight, sleep, exerciseName, weight, sets, reps, rest
 
 function UI () {}
 
-
+let counter = -1;
 //Create New Log
 UI.prototype.newLog = function(workout){
+    
+    counter++;
     const workoutLogs = document.getElementById('workoutLogs');
 
     const table = document.createElement('table');
@@ -21,7 +23,8 @@ UI.prototype.newLog = function(workout){
             <thead>
                 <tr id="heading">
                     <th>Date: ${workout.date}</th>
-                    <th>Body Weight: ${workout.bodyWeight} lb</th>
+                    <th>Body Weight: ${workout.bodyWeight}lbs</th>
+                    <th>Body Weight: ${workout.sleep}hours</th>
                     <th>X</th>
                 </tr>
                 <tr>
@@ -35,16 +38,18 @@ UI.prototype.newLog = function(workout){
                         <th>${workout.exerciseName} </th>
                         <th>${workout.weight} </th>
                         <th>${workout.sets} x ${workout.reps}</th>
-                        <th>${workout.weight}</th>
+                        <th>${workout.rest}</th>
                         <th>X</th>
                     </tr>
                 </tbody>
             </thead>
         `;
         workoutLogs.appendChild(table);
+        return counter;
 }
 
-UI.prototype.addToLog = function(workout){
+UI.prototype.addToLog = function(workout, counter){
+    console.log(counter);
     let output = '';
     output = `
     <tr>
@@ -55,28 +60,16 @@ UI.prototype.addToLog = function(workout){
         <th>X</th>
     </tr>
     `;
-    var tableBody = document.createElement('table');
+    let tableBody = document.createElement('table');
     tableBody = output;
-    document.getElementsByTagName('table')[0].insertAdjacentHTML('afterbegin', tableBody);
+
+    //NOTE, Make dynamic tables generated an array, hit the array.lenth-1 instead of counter
+    // tableBody.forEach(table => {
+    //     console.log('hi');
+    // });
+    document.getElementsByTagName('table')[counter].insertAdjacentHTML('afterbegin', tableBody);
     
 }
-
-document.getElementById('addButton').addEventListener('click', function (e) {
-    const ui = new UI;
-    const workout = new Workout;
-    workout.exerciseName = document.getElementById('exerciseName').value,
-        workout.weight = document.getElementById('weight').value,
-        workout.sets = document.getElementById('sets').value,
-        workout.reps = document.getElementById('reps').value,
-        workout.rest = document.getElementById('rest').value;
-
-    ui.addToLog(workout);
-    e.preventDefault();
-    ui.clearFields();
-    
-});
-
-
 
 
 UI.prototype.clearFields = function(){
@@ -88,26 +81,51 @@ UI.prototype.clearFields = function(){
     reps = document.getElementById('reps').value = '';
     rest = document.getElementById('rest').value = '';
     sleep = document.getElementById('sleep').value = '';
-    newEntry = document.getElementById('newEntry').checked = false;
 }
 
 //Event listeners
 document.getElementById('workout-form').addEventListener('submit', function(e){
+
     const workout = new Workout,
         ui = new UI;
 
         workout.date = document.getElementById('date').value,
         workout.bodyWeight = document.getElementById('bodyWeight').value,
+        workout.sleep = document.getElementById('sleep').value,
         workout.exerciseName = document.getElementById('exerciseName').value,
         workout.weight = document.getElementById('weight').value,
         workout.sets = document.getElementById('sets').value,
         workout.reps = document.getElementById('reps').value,
         workout.rest = document.getElementById('rest').value;
 
-    console.log(date, bodyWeight, exerciseName, weight, sets, reps, rest);
+   
+        if (workout.date === '' || workout.bodyWeight === '' || workout.sleep === '') {
+            alert('Date, Weight & sleep must be filled out to create a new log, to add to an exisiting log, click the second button')
+        } else {
+            ui.newLog(workout);
+            ui.clearFields();
+        }
+        e.preventDefault();
+});
 
-    ui.newLog(workout);
-    ui.clearFields();
-    e.preventDefault();
-
+document.getElementById('addButton').addEventListener('click', function (e) {
+    const ui = new UI;
+    const workout = new Workout;
+    workout.exerciseName = document.getElementById('exerciseName').value,
+        workout.weight = document.getElementById('weight').value,
+        workout.sets = document.getElementById('sets').value,
+        workout.reps = document.getElementById('reps').value,
+        workout.rest = document.getElementById('rest').value,
+        workout.sleep = document.getElementById('sleep').value;
+        workout.date = document.getElementById('date').value;
+        workout.bodyWeight = document.getElementById('bodyWeight').value;
+        console.log(workout.sleep);
+        if (workout.date === '' & workout.bodyWeight === '' & workout.sleep === '') {  
+            ui.addToLog(workout, counter);
+            ui.clearFields();
+            
+        } else {
+            alert("You've entered a date/time/sleep value, if you want to create a new log, please hit the submit button instead.");
+        }
+        e.preventDefault();
 });
